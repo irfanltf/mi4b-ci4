e0d1ce9109334fc7835da04c14048f1c4e4774aa
irfanltf <iluthfi469@gmail.com>
2022-05-25 08:13:55 +0700

    minggu 11

---
+++ app/Views/about/index.php
@@ -0,0 +1,82 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <title>Curriculum Vitae</title>
+</head>
+
+<body>
+    <h1>Curriculum Vitae</h1>
+    <h2>Biodata : </h2>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>Muhammad Irfan Lutfi</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td>Jl Haji Komarudin, Gang Damai No. 2</td>
+        </tr>
+        <tr>
+            <td>Umur</td>
+            <td>:</td>
+            <td>22</td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td>
+            <td>:</td>
+            <td>Lampung Timur</td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td>
+            <td>:</td>
+            <td>19-April-1980</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>:</td>
+            <td>Laki-laki</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td>Irfan@polinela.ac.id</td>
+        </tr>
+        <tr>
+            <td>No Hp</td>
+            <td>:</td>
+            <td>085273434645</td>
+        </tr>
+    </table>
+
+    <h2>Riwayat Pendidikan</h2>
+    <table>
+        <tr>
+            <td>2017-2020</td>
+            <td>Politeknik Negeri Lampung - Program Studi Manajemen Informatika</td>
+        </tr>
+    </table>
+
+
+    <h2>Pengalaman</h2>
+    <table>
+        <tr>
+            <td>1 Maret 2020 - 1 Mei 2020</td>
+            <td>Magang - PT CMBLABS Malang, Jawa Timur</td>
+        </tr>
+        <tr>
+            <td>28 November 2020 - Sekarang</td>
+            <td>Teknisi Laboratorium - Politeknik Negeri Lampung</td>
+        </tr>
+    </table>
+
+</body>
+<a href="/home/index">Pindah ke Halaman Utama</a>
+
+</html>
