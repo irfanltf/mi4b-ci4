fbff6ddfcbe926900bbe72aff8975527c8fbed03
Muhammad Irfan Lutfi <48278734+irfanltf@users.noreply.github.com>
2022-05-25 11:58:08 +0700

    Minggu 11

---
+++ app/Views/about/cv.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Curriculum Vitae</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container-fluid p-5 bg-warning">
+	<div class="row d-flex justify-content-center ">
+		<div class="col-lg-12 col-xs-12">
+			<div class="text-center">
+			  <img src="/assets/img/1.jpg" class="rounded rounded-circle" alt="..." width="25%">
+			<h1>Muhammad Irfan Lutfi</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row justify-content-center ">
+		<div class="col-6 text-center">
+			<h2 class="pt-3">Riwayat Pendidikan</h2>
+			 <div class="mb-3 row">
+			    <label for="staticEmail" class="col-sm-2 col-form-label">POLINELA</label>
+			    <div class="col-sm-10">
+			      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="2017-2020">
+			    </div>
+			  </div>
+			  <div class="mb-3 row">
+			    <label for="staticEmail" class="col-sm-2 col-form-label">SMA</label>
+			    <div class="col-sm-10">
+			      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="2014-2017">
+			    </div>
+			  </div>
+		</div>
+		<div class="col-6 text-center">
+			<h2 class="pt-3">Detail Informasi</h2>
+			<table class="table table-hover">
+				<tr>
+					<td>Alamat</td>
+					<td>:</td>
+					<td>Bandar Lampung</td>
+				</tr>
+				<tr>
+					<td>Kontak</td>
+					<td>:</td>
+					<td>085xxxxx</td>
+				</tr>
+			</table>
+		</div>		
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<h2 class="pt-4 text-center">Pengalaman Kerja</h2>
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="..." class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="..." class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="..." class="card-img-top" alt="...">
+			  <div class="card-body">
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer class="bg-warning k">
+	<p>Developed By xxxxxxx</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
